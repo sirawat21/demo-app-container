7efbf220b6e2f4ccc440bc098af5fe6149eb1f97
Sira <sirawat21@gmail.com>
2020-07-13 00:11:48 +1000

    draft

--- api/src/index.js
+++ api/src/index.js
@@ -4,10 +4,12 @@ class test {
     constructor() {
         this.os = os.version()
     }
+
     getOS = () => this.os
 }
 
 const testObj = new test()
-console.log("-+".repeat(40))
+
+console.log('-+'.repeat(40))
 console.log(testObj.getOS())
-console.log("-+".repeat(40))
+console.log('-+'.repeat(40))

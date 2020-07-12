ac05ce2ebe0b1b26bd25177e7548f7178ca6f6fc
Sira <sirawat21@gmail.com>
2020-07-12 14:48:40 +1000

    draft project

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+import os from "os"
+
+class test {
+    constructor() {
+        this.os = os.version()
+    }
+    getOS = () => this.os
+}
+
+const testObj = new test()
+console.log("-+".repeat(40))
+console.log(testObj.getOS())
+console.log("-+".repeat(40))

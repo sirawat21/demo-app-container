cc35aa2289f67f7f8af3053b2ac1ffb2c9547a0c
Sira <sirawat21@gmail.com>
2020-07-20 19:36:56 +1000

    lapse 2 api fetch create data

--- lapse-002/api/src/index.js
+++ lapse-002/api/src/index.js
@@ -1,11 +1,28 @@
-import os from "os"
+import express from "express"
 
-class test {
-    constructor() {
-        this.os = os.version()
+const api = express()
+
+api.use(express.json())
+
+let messages = []
+
+api.get("/api/messages/", (req, res) => {
+    res.status(500).send(messages)
+})
+
+api.get("/api/messages/:id", (req, res) => {
+    const searchedMessage = messages.filter(message => message.id == req.params.id)
+    res.status(500).send(searchedMessage)
+})
+
+api.post("/api/messages", (req, res) => {
+    const message = {
+        id: (messages.length > 0) ? messages.slice(-1)[0].id + 1 : 1,
+        text: req.body.text
     }
-    getOS = () => this.os
-}
+    messages.push(message)
+    res.status(500).send(message)
+})
 
-const testObj = new test()
-console.log(testObj.getOS())
+
+api.listen(3030, () => console.log('API-LISTENING: 3030'))

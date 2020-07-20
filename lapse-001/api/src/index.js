9f378d4e122ad3bf73d9c9d5b097f97c758e71ad
Sira <sirawat21@gmail.com>
2020-07-20 21:22:33 +1000

    add feature api fetdate and search

--- lapse-001/api/src/index.js
+++ lapse-001/api/src/index.js
@@ -5,8 +5,15 @@ api.use(express.json())
 
 let messages = []
 
-api.get("/api/messages", () => {
+api.get("/api/messages", (req, res) => {
     res.status(500).send(messages)
 })
+
+api.get("/api/messages/:id", (req, res) => {
+    const searchedMessage = []
+    messages.filter(message => req.params.id == message.id)
+    res.status(500).send(searchedMessage)
+})
+
 
 api.listen(3030, () => console.log("API-LISTENING: 3030"))

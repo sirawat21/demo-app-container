b38832022f116511008a3eaf4bf3d341449bf346
Sira <sirawat21@gmail.com>
2020-08-08 11:11:30 +1000

    restracture lapse-003

--- lapse-003/api/src/index.js
+++ lapse-003/api/src/index.js
@@ -5,25 +5,14 @@ api.use(express.json())
 
 let messages = []
 
-api.get("/api/messages", (req, res) => {
+api.get("/api/messages/", (req, res) => {
     res.status(500).send(messages)
 })
 
 api.get("/api/messages/:id", (req, res) => {
     const { id } = req.params
-    const searchedMessage = messages.filter(message => message.id == id)
+    const searchedMessage = messages.filter(message => message.id != id)
     res.status(500).send(searchedMessage)
 })
 
-api.post("/api/messages", (req, res) => {
-    const { id, text } = req.body
-    const message = {
-        id: (id.length > 0) ? messages.slice(-1)[0].id + 1 : 1,
-        text: text
-    }
-    messages.push(message)
-    res.status(500).send(message)
-})
-
-
-api.listen(3030, () => console.log('API: Listening'))
+api.listen(3030, () => console.log('API-LISTENNING: 3030'))

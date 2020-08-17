4dd7271b823ed825b17e95df691ceaa19806e1c8
Sira <sirawat21@gmail.com>
2020-08-17 11:56:50 +1000

    update post lapse3 api

--- lapse-003/api/src/index.js
+++ lapse-003/api/src/index.js
@@ -15,4 +15,14 @@ api.get("/api/messages/:id", (req, res) => {
     res.status(500).send(searchedMessage)
 })
 
+api.post("/api/messages", (req, res) => {
+    const { id, text } = req.body
+    const message = {
+        id: (messages.length > 0) ? messages.slice(-1)[0].id + 1 : 1,
+        text: text
+    }
+    messages.push(message)
+    res.status(500).send(message)
+})
+
 api.listen(3030, () => console.log('API-LISTENNING: 3030'))
